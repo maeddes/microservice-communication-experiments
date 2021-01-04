85aa236bc63b85b9a79bbaa82ee29e527f9d1104
Matthias Haeussler <maeddes@gmail.com>
2021-01-04 09:09:16 +0100

    Generic Hello World

--- todolistquarkuspanache/src/main/java/de/maeddes/GreetingResource.java
+++ todolistquarkuspanache/src/main/java/de/maeddes/GreetingResource.java
@@ -4,13 +4,13 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+@Path("/hello")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello Quarkus World!";
     }
 
 }
